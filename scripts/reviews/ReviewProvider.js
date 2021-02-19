779a2ef6c5d7531be07bc915c631cf54b7811a71
ericberringer <eric.berringer@gmail.com>
2021-02-19 10:09:03 -0600

    Populated our Review Provider after creating all of our review scripts components.

---
+++ scripts/reviews/ReviewProvider.js
@@ -0,0 +1,45 @@
+let reviews = []
+
+
+const eventHub = document.querySelector(".container")
+
+
+export const getReviews = () => {
+    return fetch(`${bakeryAPI.baseURL}/reviews`)
+    .then(response => response.json())
+    .then(parsedReviews => {
+        console.log(parsedReviews)
+        reviews = parsedReviews
+    })
+}
+
+export const useReviews = () => {
+    return reviews.slice()
+}
+
+export const saveReviews = review => {
+    let stringifiedObj = JSON.stringify(review)
+    return fetch('http://localhost:8088/reviews', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: stringifiedObj
+    })
+    .then(getReviews) // fetch the notes collection containing the newly added note
+    .then(dispatchStateChangeEvent) // tell any component listening that the notes state has been updated
+}
+
+
+const dispatchStateChangeEvent = () => {
+    const reviewStateChanged = new CustomEvent("reviewStateChanged")
+
+    eventHub.dispatchEvent(reviewStateChanged)
+}
+
+export const deleteReview = reviewId => {
+    return fetch(`http://localhost:8088/notes/${reviewId}`, {
+        method: "DELETE"
+    })
+        .then(getReviews)
+}
