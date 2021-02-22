0dd19c73a5f99e93df926e7f07076aef594f1686
Dylan Morris <dylanrobertmorris@gmail.com>
2021-02-22 14:41:13 -0600

    added Review function to review.js which renders customer ratings and reviews to dom

--- scripts/reviews/Review.js
+++ scripts/reviews/Review.js
@@ -1,9 +1,17 @@
+import { authHelper } from '../auth/aithHelper.js'
+import { getCustomer } from '../customers/CustomerProvider.js'
+import { deleteReview, useReviews } from "./ReviewProvider.js"
 
+const eventHub = document.querySelector('#container')
+const reviewModalElement = document.querySelector('.reviewContainer')
 
-export const Review = () => {
+export const Review = (reviewWithCustomer) => {
     return `
-    <div class="review">
-        <p class="reviewRating" id="review__rating--${reviewWithCustomer}"></p>
-    </div>
-    `
+        <div class="review">
+            <p class="review__rating" id="review__rating--${reviewWithCustomer.review.id}">${reviewStars[reviewWithCustomer.review.rating]}</p>
+            <strong>
+                <p class="review__name">${reviewWithCustomer.customer.name}</p>
+            </strong>
+        </div>
+            `   
 }
