d3e75b28b7f3ab4579cdc800d63b418f01bcdfab
Dylan Morris <dylanrobertmorris@gmail.com>
2021-02-22 15:01:38 -0600

    factored review.js so it renders customers, ratings, and reviews under the menu items

--- scripts/reviews/Review.js
+++ scripts/reviews/Review.js
@@ -1,17 +1,28 @@
-import { authHelper } from '../auth/aithHelper.js'
+import { authHelper } from '../auth/authHelper.js'
 import { getCustomer } from '../customers/CustomerProvider.js'
 import { deleteReview, useReviews } from "./ReviewProvider.js"
 
 const eventHub = document.querySelector('#container')
 const reviewModalElement = document.querySelector('.reviewContainer')
 
+const reviewStars = [
+    "",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5"
+]
+
+
 export const Review = (reviewWithCustomer) => {
     return `
         <div class="review">
-            <p class="review__rating" id="review__rating--${reviewWithCustomer.review.id}">${reviewStars[reviewWithCustomer.review.rating]}</p>
+            <p class="review__rating" id="review__rating--${reviewWithCustomer.review.id}"> Customer Rating: ${reviewWithCustomer.review.rating}</p>
             <strong>
-                <p class="review__name">${reviewWithCustomer.customer.name}</p>
+                <p class="review__name">Customer Name: ${reviewWithCustomer.customer.name}</p>
             </strong>
+            <p>Review: ${reviewWithCustomer.review.text}</p>
         </div>
             `   
 }
